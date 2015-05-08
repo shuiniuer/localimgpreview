1686cbd8b5dd761cb6b3182b97061b1842724ab5
水牛儿 <shuiniuer27@163.com>
2015-05-08 21:49:07 +0800

    Update localimgpreview.js
    修改默认erro方法

--- localimgpreview.js
+++ localimgpreview.js
@@ -7,7 +7,7 @@
             "height": 310,
             "defaultImg": "http://i00.c.aliimg.com/cms/upload/2014/704/088/1880407_1917015703.jpg",
             "error": function(){
-                alert('图片异常，请检查！');
+                alert('图片格式常，请检查！');
             }
         };
         config = $.extend(config, configObj);
